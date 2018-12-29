9207447d51ace53773ffd281ea36260c1655b25a
Egor Timashev <metallagetemp@gmail.com>
2018-12-29 11:23:52 +0300

    Добавьте файлы проекта.

---
+++ DirectCertInstall/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DirectCertInstall
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
